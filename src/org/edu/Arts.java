a7d3f311dcc407f07d4f1ad39ac1f67541e8b912
Pragashvel <velpragash27595@gmail.com>
2022-04-23 07:24:16 +0530

    add

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,26 @@
+package org.edu;
+
+public class Arts extends Education {
+	public void bsc() {
+		System.out.println("Physics");
+	}
+	public void bEd() {
+		System.out.println("Chemistry");
+	}
+	public void bA() {
+		System.out.println("English");
+	}
+	public void ug() {
+		System.out.println("B.E");
+	}
+	public void pg() {
+		System.out.println("M.com");
+	}
+	public static void main(String[] args) {
+		Arts l = new Arts();
+		l.bsc();
+		l.bEd();
+		l.bA();
+		l.ug();
+		l.pg();
+	}}
---
+++ src/org/edu/Education.java
@@ -0,0 +1,12 @@
+package org.edu;
+
+public class Education {
+	public void ug() {
+		System.out.println("B.com");
+	}
+	public void pg() {
+		System.out.println("M.com");
+		
+	}
+	}
+
